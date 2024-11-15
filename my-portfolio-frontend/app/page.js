630ff6a23e75921a59c481e1ba94bfe135e7d179
Sahitya hp <neupanesahitya1@gmail.com>
2024-11-14 22:16:28 -0500

    added header footer component

--- my-portfolio-frontend/app/page.js
+++ my-portfolio-frontend/app/page.js
@@ -1,13 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <header>header</header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        main
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        footer
-      </footer>{" "}
-    </>
-  );
+  return <div></div>;
 }
