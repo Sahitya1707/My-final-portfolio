6a392857c834ab7d6dbdd9eb6d06e93e3d08e52c
Sahitya hp <neupanesahitya1@gmail.com>
2024-11-14 22:56:36 -0500

    added colors root variable

--- my-portfolio-frontend/tailwind.config.js
+++ my-portfolio-frontend/tailwind.config.js
@@ -7,9 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      // adding root color read it here https://stackoverflow.com/questions/64872861/how-to-use-css-variables-with-tailwind-css
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "rgb(var(--primary-clr)/<alpha-value>)",
+        textClr: "rgb(var(--Clr-text)/<alpha-value>)",
+        navClr: "rgb(var(--nav-clr)/<alpha-value>)",
+        bodyClr: "rgb(var(--body-clr)/<alpha-value>)",
+        colorFive: "rgb(var(--clr-5)/<alpha-value>)",
+        colorSix: "rgb(var(--clr-6)/<alpha-value>)",
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
       },
     },
   },
