0500e344bf5193c69be833e4ad3ce65d9b0ae272
Sahitya hp <neupanesahitya1@gmail.com>
2025-01-18 02:01:24 -0500

    MADE THE next public

--- my-portfolio-frontend/app/utils/secret.js
+++ my-portfolio-frontend/app/utils/secret.js
@@ -1,2 +1,2 @@
-export const backendURI = process.env.API_URL;
-export const adminURI = process.env.ADMIN_URL;
+export const backendURI = process.env.NEXT_PUBLIC_API_URL;
+export const adminURI = process.env.NEXT_PUBLIC_ADMIN_URL;
